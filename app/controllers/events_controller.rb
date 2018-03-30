2d0b2d640837f6a27077ca562d8981360f4bd752
AgeMagi <muhhabibih@gmail.com>
2018-03-30 10:43:38 +0700

    Sudah bisa css terpisah

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -74,6 +74,6 @@ class EventsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def event_params
-      params.require(:event).permit(:title, :narasi, :dilaksanakan, photos_attributes: [:id, :event_id, :foto])
+      params.require(:event).permit(:title, :narasi, :dilaksanakan, :dilaksanakan, photos_attributes: [:id, :event_id, :foto])
     end
 end
