58b5ee5fd5fa55f5173c9c66becb223788070ee7
AgeMagi <muhhabibih@gmail.com>
2018-04-05 18:45:45 +0700

    buat heroku

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,13 +7,14 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.precompile += %w( alumnus.css )
-Rails.application.config.assets.precompile += %w( events.css )
-Rails.application.config.assets.precompile += %w( participants.css )
-Rails.application.config.assets.precompile += %w( photos.css )
-Rails.application.config.assets.precompile += %w( scaffolds.css )
-Rails.application.config.assets.precompile += %w( users.css )
-Rails.application.config.assets.precompile += %w( sessions.css )
+# Rails.application.config.assets.precompile += %w(*)
+# Rails.application.config.assets.precompile += %w( alumnus.css )
+# Rails.application.config.assets.precompile += %w( events.css )
+# Rails.application.config.assets.precompile += %w( participants.css )
+# Rails.application.config.assets.precompile += %w( photos.css )
+# Rails.application.config.assets.precompile += %w( scaffolds.css )
+# Rails.application.config.assets.precompile += %w( users.css )
+# Rails.application.config.assets.precompile += %w( sessions.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
