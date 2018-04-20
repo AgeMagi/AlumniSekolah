6348771a6dd14c6ca0e9773175726ffed65fb405
ahmadfahmip <afahmi3@yahoo.co.id>
2018-04-20 20:01:23 +0700

    solve loggedin

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-class Event < ApplicationRecord
-  has_many :photos
-  has_many :participants
-  validates :photos, presence: true
-  validates_associated :photos
-  validates_associated :participants
-  validates :dilaksanakan, presence: true
-  validates :narasi, presence: true
-end
-=======
 class Event < ApplicationRecord
   has_many :photos
   has_many :participants
@@ -16,4 +5,3 @@ class Event < ApplicationRecord
   validates :dilaksanakan, presence: true
   validates :narasi, presence: true
 end
->>>>>>> 72853ed04a13cf9cc3a83be13228c45c46b8bc87
